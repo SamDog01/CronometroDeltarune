b2187d73d165b4f1d9963f7e16c0548189c6feae
samuelexpedito <samuelexpedito2019@gmail.com>
2025-08-29 01:58:06 -0300

    Adicionado um botão que troca

---
+++ cronometro.js
@@ -0,0 +1,12 @@
+    window.onload = () => {
+        const javaTest = document.getElementById("Elemento_Teste");
+        javaTest.classList.add("Test_Text");
+
+        
+        const ButColor = document.querySelector(".ClickTro");
+        ButColor.addEventListener("click",()=>{
+            ButColor.style.backgroundColor = "red";
+        });
+        
+
+};
